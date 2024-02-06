a8c43014c6c96c51341de10418f2017cc05c287f
taiyou <hiromacha1116@icloud.com>
2024-02-06 13:16:14 +0900

    複数選択1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,25 +6,46 @@ export default function Home() {
   // テスト
   const allWords = "In the realm of software development, a significant paradigm shift has been observed towards embracing cloud-native technologies. This transition is not merely a trend but a strategic move to leverage the inherent scalability, resilience, and flexibility offered by cloud platforms. As organizations migrate their infrastructures and applications to the cloud, they unlock new avenues for innovation and efficiency. This evolution is pivotal for staying competitive in today's fast-paced digital landscape, where the ability to rapidly adapt and respond to market demands is crucial for success.";
   const words = allWords.split(" ");
-  // 透明な単語のインデックスを追跡するための状態
-  const [transparentIndexes, setTransparentIndexes] = useState<number[]>([]);
+  const [isDragging, setIsDragging] = useState(false);
+  const [selectedWords, setSelectedWords] = useState<number[]>([]);
 
   const handleWordClick = (index: number) => {
-    // クリックされた単語のインデックスを透明なインデックスリストに追加
-    setTransparentIndexes(current => [...current, index]);
+    setSelectedWords(current => [...current, index]);
   }
 
+  const handleMouseDown = () => {
+    setIsDragging(true);
+    setSelectedWords([]);
+  };
+
+  const handleMouseMove = (index: number) => {
+    if (!isDragging) return;
+    setSelectedWords((prev) => {
+      // 重複選択を避けるために新しいインデックスだけを追加
+      return prev.includes(index) ? prev : [...prev, index];
+    });
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+    selectedWords.map((n) => {
+      console.log(words[n]);
+    })
+  };
+
   return (
-    <div className=" p-5 break-all">
+    <div className=" p-5 break-all" onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
       {words.map((word, index) => (
         <span 
           key={index} 
           onClick={() => handleWordClick(index)} 
+          onMouseMove={() => handleMouseMove(index)}
           style={{
             marginRight: '5px', 
             cursor: 'pointer',
             // 透明な単語のスタイル適用
-            color: transparentIndexes.includes(index) ? 'transparent' : 'inherit'
+            // color: transparentIndexes.includes(index) ? 'transparent' : 'inherit'
+            backgroundColor: selectedWords.includes(index) ? 'yellow' : 'transparent',
           }}
         >
           {word}
