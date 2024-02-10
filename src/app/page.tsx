7a4c51a56f88acd25298f10afdefa9680636624f
taiyou <hiromacha1116@icloud.com>
2024-02-10 16:23:34 +0900

    amplify追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 "use client"
 
 import SentenceComponent from "@/components/SentenceComponent"
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 export default function Home() {
   return (
     <div>
       <SentenceComponent />
+      <Authenticator>
+            {({ signOut, user }) => (
+                <div>
+                    <p>Welcome {user!.username}</p>
+                    <button onClick={signOut}>Sign out</button>
+                </div>
+            )}
+        </Authenticator>
     </div>
   )
 }
