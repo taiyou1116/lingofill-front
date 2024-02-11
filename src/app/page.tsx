4944fce76269f01164e35edc8b24c4a5c1776491
taiyou <hiromacha1116@icloud.com>
2024-02-11 12:28:05 +0900

    ユーザーのサインイン情報

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,46 @@
 "use client"
 
 import SentenceComponent from "@/components/SentenceComponent"
+import "./globals.css";
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+import awsExports from "../aws-exports";
+Amplify.configure(awsExports);
 
-export default function Home() {
+const App = () => {
+  return (
+    <Authenticator.Provider>
+      <MyApp />
+    </Authenticator.Provider>
+  );
+};
+
+const MyApp = () => {
+  const { route } = useAuthenticator((context) => [context.route]);
+
+  console.log(route);
+
   return (
     <div>
-      <SentenceComponent />
+      { route === "authenticated"
+      ?
+        <SentenceComponent />
+      : 
+        <div className=' py-5'>
+          <Authenticator>
+            {({ signOut, user }) => (
+              <div>
+                  <p>Welcome {user!.username}</p>
+                  
+                  <button onClick={signOut}>Sign out</button>
+              </div>
+            )}
+          </Authenticator>
+        </div>
+      }
     </div>
   )
-}
+};
+
+export default App;
